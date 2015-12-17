01ce5aa5535e844d22f87506da23a6a69336f677
Aleksey Storozhev <dark.storoj@gmail.com>
2015-12-17 15:50:20 +0300

    Initial Commit

---
+++ Component5-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Component5-Demo
+//
+//  Created by Alexey Storozhev on 17/12/15.
+//  Copyright © 2015 Aleksey Storozhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Component5-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Component5-Demo
+//
+//  Created by Alexey Storozhev on 17/12/15.
+//  Copyright © 2015 Aleksey Storozhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
